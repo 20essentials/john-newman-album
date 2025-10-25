5e06bdb9b265703049ffc6de779dba660fe7f6c5
DominicDev <s0l0estudi4r@gmail.com>
2025-10-25 12:29:15 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'JOHN NEWMAN ';
+export const nameSongs = [
+	"CAN YOU LOVE ME AGAIN?", /*1*/
+	"BLAME", /*2*/
+	"CAN YOU LOVE ME AGAIN? (INSTRUMENTAL)", /*3*/
+	"COME AND GET IT", /*4*/
+	"LIGHTS DOWN", /*5*/
+	"SOMETHING SPECIAL", /*6*/
+	"CALLED IT OFF", /*7*/
+	"ALICE KUCEVIC - LOVE ME AGAIN", /*8*/
+	"OUT OF MY HEAD", /*9*/
+	"I’M NOT YOUR MAN", /*10*/
+	"OLÉ", /*11*/
+	"LOSING SLEEP", /*12*/
+	"CHEATING", /*13*/
+	"GIVE YOU MY LOVE", /*14*/
+	"THE PAST", /*15*/
+	"FIRE IN ME", /*16*/
+	"NOTHING ", /*17*/
+	"EASY ", /*18*/
+	"GOODNIGHT GOODBYE", /*19*/
+	"TRIBUTE", /*20*/
+]
